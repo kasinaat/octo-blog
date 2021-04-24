1b4617edbcf8d404f1910d08a02379afb32a8913
SSK <kasinaat007@gmail.com>
2021-04-24 16:49:54 +0530

    backend template created

---
+++ global.js
@@ -0,0 +1,5 @@
+var path = require('path');
+module.exports = {
+    clientPath : path.join(__dirname ,"octo-blog-client/build"),
+    clientIndexPath : path.join(__dirname ,"octo-blog-client/build/index.html")
+};
