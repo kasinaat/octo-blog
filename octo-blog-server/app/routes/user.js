1ca04ba33c6accb66d1336ea4f503a6aedd9180f
SSK <kasinaat007@gmail.com>
2021-05-02 14:06:01 +0530

    imports updated

--- octo-blog-server/app/routes/user.js
+++ octo-blog-server/app/routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-var User = require('../../models/user');
+var User = require('../models/user');
 
 router.get('/', async(req, res) => {
     try {
